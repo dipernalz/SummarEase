339ba0e72bc567e1f2cb1aa36b8fad27fed8c5f2
markhamilton28 <markhamilton@vt.edu>
2023-03-25 14:29:48 -0400

    search button sends post request

--- client/src/components/searchbar/Searchbar.js
+++ client/src/components/searchbar/Searchbar.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import "./searchbar.css";
 
 export default function Searchbar() {
+
+  const handleSubmit = (e, t) => {
+    e.preventDefault();
+    console.log("url is " + t);
+    fetch('http://localhost:3000/api/product', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: {url: t}
+    })
+  }
   return (
-    <form className="search">
-      <input type="text" className="searchTerm" placeholder="Enter listing url"/>
-      <button type="submit" className="searchButton">
-            <SearchIcon className="searchIcon" />
-      </button>
-    </form>
+    <div>
+      <form className="search" onSubmit={(e) => {
+        handleSubmit(e, document.getElementById("searchTerm").value);
+      }}>
+        <input 
+          id="searchTerm"
+          type="text" 
+          className="searchTerm"
+          placeholder="Enter listing url"
+          required
+        />
+        <button className="searchButton">
+              <SearchIcon className="searchIcon" />
+        </button>
+      </form>
+    </div>
   )
 }
