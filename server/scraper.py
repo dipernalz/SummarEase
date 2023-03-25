10b19d079e711d65ea92561aca8403ede37363c7
dipernalz <lzdiperna@gmail.com>
2023-03-25 15:16:21 -0400

    Scrape both positive and critical reviews

--- server/scraper.py
+++ server/scraper.py
@@ -4,20 +4,25 @@ import sys
 import re
 
 
+review_url = f"https://www.amazon.com/product-reviews"
+review_types = ["positive", "critical"]
+
+
 def get_product_id(url):
     search = re.search(r"[A-Z0-9]{10}", url)
     return search.group(0) if search is not None else None
 
 
-def get_source(url):
-    source = None
+def get_source(urls):
+    source = []
 
     try:
         options = webdriver.FirefoxOptions()
         options.add_argument("--headless")
         browser = webdriver.Firefox(options=options)
-        browser.get(url)
-        source = browser.page_source
+        for url in urls:
+            browser.get(url)
+            source.append(browser.page_source)
     finally:
         try:
             browser.close()
@@ -40,15 +45,19 @@ def get_reviews(soup):
 def main():
     url = sys.argv[1]
     product_id = get_product_id(url)
+    output = {"reviews": {}}
     if product_id is not None:
-        review_url = f"https://www.amazon.com/product-reviews/{product_id}"
-        source = get_source(review_url)
-        
-        soup = BeautifulSoup(source, "lxml")
-        reviews = get_reviews(soup)
-        output = {"status": "success", "reviews": reviews}
+        sources = get_source([
+            f"{review_url}/{product_id}?filterByStar={review_type}"
+            for review_type in review_types
+        ])
+        for i, review_type in enumerate(review_types):
+            output["reviews"][review_type] = get_reviews(
+                BeautifulSoup(sources[i], "lxml"),
+            )
+        output["status"] = "success"
     else:
-        output = {"status": "failure", "reviews": []}
+        output["status"] = "failure"
     print(output)
 
 
