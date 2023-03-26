97ff42fb250b78a3ac2a182ec875b3825a2d4fa2
Dennis Tian <dtian88@gmail.com>
2023-03-26 01:17:59 -0400

    MUI search bar

--- client/src/components/searchbar/Searchbar.js
+++ client/src/components/searchbar/Searchbar.js
@@ -1,29 +1,67 @@
 import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import styled from "styled-components";
+import { InputAdornment, IconButton } from '@mui/material';
 import "./searchbar.css";
 
+
 export default function Searchbar(props) {
-  
+  const { classes } = props;
   const handleSubmit = async (e, searchTerm) => {
     e.preventDefault();
     console.log("Searchbar calling prop fn");
     props.urlCallback(searchTerm);
   }
 
+  // const styles = createStyles({
+  //   notchedOutline: { borderColor: '#f0f !important' },
+  // });
+
+  const CustomTextField = styled(TextField)`
+  & label.Mui-focused {
+    color: #c65102;
+  }
+  & label {
+    color: #c65102;
+  }
+  & .MuiOutlinedInput-root {
+    border-radius: 20px;
+    &.Mui-focused fieldset {
+      border-color: #c65102;
+    }
+    &:hover fieldset {
+      border-color: #c65102;
+    }
+    & fieldset {
+      border-color: #c65102;
+    }
+  }
+`;
+
   return (
     <div>
       <form className="search" onSubmit={(e) => {
         handleSubmit(e, document.getElementById("searchTerm").value);
       }}>
-        <input 
+        <CustomTextField style = {{ width: "75%" }} label="Enter listing url" type="search" id="searchTerm" required InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton edge="end" className="searchButton" type="submit">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}/>
+        {/* <input 
           id="searchTerm"
           type="text" 
           className="searchTerm"
           placeholder="Enter listing url"
           required
-        />
-        <button className="searchButton">
+        /> */}
+        {/* <button className="searchButton">
           <SearchIcon className="searchIcon" />
-        </button>
+        </button> */}
       </form>
     </div>
   )
